4dda493a502e66f1178f73ace2b685c244c6bdff
omarVodiaK <omar.boudfoust@gmail.com>
2021-07-05 13:05:12 +0800

    point public-api.ts to lib/index.ts

--- projects/dynamic-form/src/public-api.ts
+++ projects/dynamic-form/src/public-api.ts
@@ -1,13 +1,4 @@
 /*
  * Public API Surface of dynamic-form
  */
-export * from './lib/dynamic-form.module';
-export * from './lib/dynamic-form.component';
-export * from './lib/dynamic-form-item/dynamic-form-item.component';
-export * from './lib/dynamic-form-modal/dynamic-form-modal.component';
-export * from './lib/dynamic-from-data/dynamic-form-data.component';
-export * from './lib/dynamic-form-media/dynamic-form-media.component';
-export * from './lib/safe.pipe';
-export * from './lib/digit-only/digit-only.directive';
-export * from './lib/form.service';
-export * from './lib/model/models';
+export * from './lib/index';
